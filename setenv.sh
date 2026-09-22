27335b1913364e0ecf74e66b7cd5c740ccc23fe3
John McMullin <jdmcmullin@aol.com>
2026-09-22 21:24:42 +0100

    Updated the setenv.sh so it can be executed from any directory and still setup the shell variables to point to the folder holding setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 # Simple IMP compiler shell wrapper
 
-echo "$0"
-echo "${PWD}"
+IMP_BASE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "running ${IMP_BASE}/setenv.sh from ${PWD}"
 
 if [ -z ${IMP_SOURCE_HOME} ]; then
-    export IMP_SOURCE_HOME="${PWD}"
-    export IMP_INCLUDE_HOME="${PWD}/include"
+    export IMP_SOURCE_HOME="${IMP_BASE}"
+    export IMP_INCLUDE_HOME="${IMP_BASE}/include"
     export IMP_FILESEP="/"
-    
 fi
 
 if [ -z ${IMP_INSTALL_HOME} ]; then
-    if [ ! -e ${PWD}/release ]; then
-        mkdir ${PWD}/release
+    if [ ! -e ${IMP_BASE}/release ]; then
+        mkdir ${IMP_BASE}/release
     fi
-    if [ ! -e ${PWD}/release/bin ]; then
-        mkdir ${PWD}/release/bin
+    if [ ! -e ${IMP_BASE}/release/bin ]; then
+        mkdir ${IMP_BASE}/release/bin
     fi
-    if [ ! -e ${PWD}/release/include ]; then
-        mkdir ${PWD}/release/include
+    if [ ! -e ${IMP_BASE}/release/include ]; then
+        mkdir ${IMP_BASE}/release/include
     fi
-    if [ ! -e ${PWD}/release/lib ]; then
-        mkdir ${PWD}/release/lib
+    if [ ! -e ${IMP_BASE}/release/lib ]; then
+        mkdir ${IMP_BASE}/release/lib
     fi
-    export IMP_INSTALL_HOME=${PWD}/release
+    export IMP_INSTALL_HOME=${IMP_BASE}/release
     export PATH=${IMP_INSTALL_HOME}/bin:${PATH}
 fi
-
-#if [ -z ${IMP_EXPORT_HOME} ]; then
-#    
-#fi
 
 if [ -z ${IMP_TOOLS_HOME} ]; then
     export IMP_TOOLS_HOME="${IMP_INSTALL_HOME}"
